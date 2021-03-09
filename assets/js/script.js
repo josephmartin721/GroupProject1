7a727ea6b5ca3253dc3c588b4c033ea7b57880ab
Joseph Martin <josephmartin721@yahoo.com>
2021-03-09 12:14:54 -0600

    changed from chuck to pure css

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,5 @@
+const RAPID_API_KEY = 'f577c81c30msh97c9a74f04d0771p1126bejsnd343a5c3de13';
+const RAPID_API_HOST = 'apidojo-yahoo-finance-v1.p.rapidapi.com';
 const top100TableBodyEl = $('#top100-tbody');
 
 // For testing purposes
@@ -28,16 +30,25 @@ const displayTop100Table = function(tickers) {
                 <td>${stock.name}</td>
                 <td>${stock.ticker}</td>
                 <td>&#36;${stock.askprice}</td>
+                <td>Available Platforms</td>
             </tr>
         `);
     }
 }
 
-var getTrendingTickers = {
-    
+// API call to get trending stock tickers
+// TODO Response error handling
+const getTrendingTickers = function(query, callback) {
+    const url = `https://apidojo-yahoo-finance-v1.p.rapidapi.com/market/get-trending-tickers?region=US`;
+
+    fetch(url, {
+        headers: {
+            'x-rapidapi-key': RAPID_API_KEY,
+	        'x-rapidapi-host': RAPID_API_HOST,
+	        'useQueryString': true
+        }
+    })
+    .then(response => response.json())
+    .then(data => console.log(data))
+    .catch(error => console.log(error));
 }
-
-// TODO Api calls to get stock data
-// TODO Display stock ticker
-// TODO Handle stock symbol search form
-
