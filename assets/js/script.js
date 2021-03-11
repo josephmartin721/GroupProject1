77714316c51fec94250baab28cf3393b06da0dfd
Hagen DeRouen <hagend87@gmail.com>
2021-03-11 12:57:12 -0600

    Add function to get available markets for a specific stock symbol.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,12 @@
 const RAPID_API_KEY = 'f577c81c30msh97c9a74f04d0771p1126bejsnd343a5c3de13';
-const RAPID_API_HOST = 'apidojo-yahoo-finance-v1.p.rapidapi.com';
+const RAPID_API_YAHOO_HOST = 'apidojo-yahoo-finance-v1.p.rapidapi.com';
+const RAPID_API_FIDELITY_HOST = 'fidelity-investments.p.rapidapi.com';
 const top100TableBodyEl = $('#top100-tbody');
+const symSearch = $('#input').val();
+const fidelity = {
+        name: 'Fidelity Investments',
+        url: 'https://www.fidelity.com/'
+    }
 
 // For testing purposes
 const tickers = [
@@ -45,7 +51,6 @@ const getTrendingTickers = function(query, callback) {
         headers: {
             'x-rapidapi-key': RAPID_API_KEY,
 	        'x-rapidapi-host': RAPID_API_HOST,
-	        'useQueryString': true
         }
     })
     .then(response => response.json())
@@ -56,19 +61,52 @@ const getTrendingTickers = function(query, callback) {
 // TODO Display scrolling stock ticker
 // TODO Handle stock symbol search form
 
-var symSearch = $('#input').val()
-var symSearch = 'AAPL'
-
 const searchSymbol = function(symbol) {
     
 fetch("https://apidojo-yahoo-finance-v1.p.rapidapi.com/market/v2/get-quotes?region=US&symbols=" + symbol, {
 	"method": "GET",
 	"headers": {
 		"x-rapidapi-key": RAPID_API_KEY,
-		"x-rapidapi-host": RAPID_API_HOST
+		"x-rapidapi-host": RAPID_API_YAHOO_HOST
    }})
    .then(response => response.json())
    .then(data => console.log(data))
    .catch(err => console.log(err))
 }
-searchSymbol ('AAPL')
+
+
+// Fidelity is returning xml data!
+const getAvailablePlatforms = function(symbol, callback) {
+    let platforms = [];
+
+    const url = `https://fidelity-investments.p.rapidapi.com/quotes/get-details?symbols=${symbol}`
+
+    fetch(url, {
+        headers: {
+            'x-rapidapi-key': RAPID_API_KEY,
+            'x-rapidapi-host': RAPID_API_FIDELITY_HOST,
+        }
+    })
+    .then(response => response.text())
+    .then(function(data) {
+        console.log(data);
+        const parser = new DOMParser();
+        // Parses xml string into DOM tree
+        const $dom = $(parser.parseFromString(data, 'application/xml'));
+
+        if ($dom.find('STATUS').find('ERROR_CODE').text() === '0') {
+            platforms.push(fidelity);
+        }
+
+        callback(platforms);
+    })
+    .catch(function(err) {
+        console.log(err);
+        callback(platforms);
+    });
+
+    
+    
+}
+
+
